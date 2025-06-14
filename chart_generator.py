af19824c1cc55364096c865bcea5fe63d60ef8e0
alkahfi23 <58243832+alkahfi23@users.noreply.github.com>
2025-06-14 14:17:13 +0700

    Update chart_generator.py

--- chart_generator.py
+++ chart_generator.py
@@ -78,14 +78,7 @@ def normalize_symbol(symbol):
         if converted in contracts:
             return converted
     return None
-
-import json
-import time
-import threading
-import websocket
-import pandas as pd
-
-
+    
 def get_klines(symbol="BTC_USDT", intervals=["1m", "5m", "15m"], contract_type="usdt", duration=20):
     url = f"wss://fx-ws.gateio.ws/v4/ws/{contract_type}"
     klines_by_interval = {interval: [] for interval in intervals}
